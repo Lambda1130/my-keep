133b4df24915fb9bb2310fb9822533aabadb1b1f
Lambda1130 <saku0120etou@ybb.ne.jp>
2023-11-02 15:29:53 +0900

    up

---
+++ homewrok/my study/studycpp.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	/*for (int i= 5; i < 10; i -= 2)
+	{
+		cout << i << "";
+	}
+	cout << endl;*/
+
+	int a = 0;
+	for (;;)
+	{
+		cout << a << "";
+		++a;
+	}
+	cout << endl;
+}
