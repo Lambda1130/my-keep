00bbb92b8deddcea93fb5ffc9c2c4fdcf6499752
Lambda1130 <saku0120etou@ybb.ne.jp>
2023-11-17 17:04:23 +0900

    up

--- homewrok/my study/studycpp.cpp
+++ homewrok/my study/studycpp.cpp
@@ -3,24 +3,113 @@
 #include<stdio.h>
 #include<cmath>
 #include <bits/stdc++.h>
+#include<vector>
 using namespace std;
 
-int main(void)
-{
-    std::string n;
-    std::cin >> n;
-    int m = 0;
-    int s = 0;
 
-    for (int i = n.length() - 1; i >= 0; --i) 
-    {
-        m += (n[i] ) * std::pow(2, s);
-        ++s;
-    }
-    std::cout << m << std::endl;
-    return 0;
-   
+    
+int main(void) 
+{
+        // 成績の入力
+        int grade;
+        std::cin >> grade;
+        // プレゼント候補の入力
+        std::vector<std::string> presents(3);
+        for (int i = 0; i < 3; ++i)
+        {
+            std::cin >> presents[i];
+        }
+        // 成績に応じてソートし、対応するプレゼントを出力
+        if (grade == 3) 
+        {
+            std::cout << presents[2] << std::endl;
+        }
+        else if (grade == 2)
+        {
+            std::cout << presents[1] << std::endl;
+        }
+        else
+        {
+            std::cout << presents[0] << std::endl;
+        }
+        
+        return 0;
+    
 }
+
+//int main(void)
+//{
+//    int n;
+//    cin >> n;
+//
+//    string m(n, '*');
+//
+//        cout << ""<<m << endl;
+//
+//    return 0;
+
+
+//int main(void)
+//{
+//    int n;
+//    cin >> n;
+//    int m = 24;
+//
+//    int a = n / m;
+//    if (n % m == 0)
+//    {
+//        cout << "" << a << endl;
+//    }
+//    else
+//    {
+//        cout << "" << a + 1 << endl;
+//    }
+//    return 0;
+//}
+
+
+
+//int main(void)
+//{
+//    int N;
+//    std::cin >> N;
+//    std::string n, m;
+//    bool isSucceeded = true;
+//    for (int i = 0; i < N; ++i)
+//    {
+//        std::cin >> m;
+//        if (i > 0 && n.back() != m.front())
+//        {
+//            std::cout << n.back() << " " << m.front() << std::endl;
+//            isSucceeded = false;
+//            break;
+//        }
+//        n = m;
+//    }
+//    if (isSucceeded)
+//    {
+//        std::cout << "Yes" << std::endl;
+//    }
+//    return 0;
+//}
+
+
+//int main(void)
+//{
+//    std::string n;
+//    std::cin >> n;
+//    int m = 0;
+//    int s = 0;
+//
+//    for (int i = n.length() - 1; i >= 0; --i) 
+//    {
+//        m += (n[i]-'0') * std::pow(2, s);
+//        ++s;
+//    }
+//    std::cout << m << std::endl;
+//    return 0;
+//   
+//}
 
 //int main(void)
 //{
